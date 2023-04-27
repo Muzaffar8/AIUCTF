eca8e4ac1a78075ad879f0f4d3692bfe41d21e45
Vladimir Baltiev <v.golem2288@gmail.com>
2023-04-28 00:18:57 +0600

    project

---
+++ frontend/src/store/configureStore.js
@@ -0,0 +1,52 @@
+import {combineReducers} from "redux";
+// import {loadFromLocalStorage, saveToLocalStorage} from "./localStorage";
+import {configureStore} from "@reduxjs/toolkit";
+import axiosApi from "../axiosApi";
+import thunk from "redux-thunk";
+import usersSlice from "./slices/usersSlice";
+import {loadFromLocalStorage, saveToLocalStorage} from "./localStorage";
+import challengesSlice from "./slices/challengesSlice";
+import competitionsSlice from "./slices/competitionsSlice";
+
+const rootReducer = combineReducers({
+    users: usersSlice.reducer,
+    challenges: challengesSlice.reducer,
+    competitions: competitionsSlice.reducer,
+});
+
+const persistedState = loadFromLocalStorage();
+const middleware = [thunk];
+
+const store = configureStore({
+    reducer: rootReducer,
+    middleware,
+    devTools: true,
+    preloadedState: persistedState,
+});
+
+
+store.subscribe(() => {
+    saveToLocalStorage({
+        users: {
+            user: store.getState().users.user,
+        },
+    });
+});
+
+axiosApi.interceptors.request.use(config => {
+    try {
+        config.headers['Authorization'] = store.getState().users.user.token;
+    } catch (e) {}
+
+    return config;
+});
+
+axiosApi.interceptors.response.use(res => res, e => {
+    if (!e.response.data) {
+        e.response = {data: {global: 'No internet!'}};
+    }
+
+    throw e;
+});
+
+export default store;
